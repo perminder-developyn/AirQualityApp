c2355592c41d26ecacf18f9875085951f04f0f2b
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-14 16:17:16 +0100

    minor styling done

---
+++ src/components/Countries.svelte
@@ -0,0 +1,139 @@
+<div class="paginate-buttons">
+	<button on:click={previous}> Previous </button>
+	Page {page}
+	<button on:click={next}> Next </button>
+</div>
+<div class="country-list">
+	{#each worldData as countryData}
+		<button class="countries" on:click={seeCities(countryData)}>{countryData.name}</button>
+	{/each}
+</div>
+<div class="city-air-stats">
+	<th>
+		{currentCity}
+	</th>
+	{#each currentCityInfo as info}
+		<tr>{info[0]} : {parseFloat(info[1]).toFixed(2)} {info[2]}</tr>
+	{/each}
+</div>
+<div class="cities">
+	<h2>{currentCountry}</h2>
+	{#if cities.length > 0}
+		{#each cities as city}
+			{#if city.city != "N/A"}
+				<button class="city-list-item" on:click={cityData(city)}
+				>
+					{city.city}
+				</button>
+			{/if}
+		{/each}
+	{/if}
+</div>
+
+<script>
+import axios from 'axios';
+let page = 1;
+let count = 0;
+let worldData = [];
+let cities = [];
+let cityInformation = [];
+let currentCountry = '';
+let currentCity = '';
+let currentCityInfo = [];
+let averageData = [];
+
+const updateCountries = () => {
+	axios.get(`https://api.openaq.org/v2/countries?limit=20&page=${page}`)
+		.then(function(response){
+			worldData = response.data.results;
+		})
+};
+
+const seeCities = (e) => {
+	axios.get(`https://api.openaq.org/v2/cities?&country_id=${e.code}`)
+		.then(function(response){
+			cities = (response.data.results);
+			currentCity = '';
+			currentCityInfo = [];
+			currentCountry = e.name;
+		})
+}
+const cityData = (e) => {
+	axios.get(`https://api.openaq.org/v2/locations?city=${e.city}`)
+		.then(function(response){
+			currentCityInfo = [];
+			averageData = [];
+			currentCity = e.city;
+			cityInformation = response.data.results;
+			cityInformation.forEach(element => {
+				element.parameters.forEach(i => {
+					currentCityInfo.push([i.displayName, i.average, i.unit])
+				})
+			})
+			currentCityInfo.sort()
+			for (let i = 0; i <= currentCityInfo.length; i++) {
+				let element = currentCityInfo
+				if (element[i+1]) {
+					if (element[i][0]===element[i+1][0]) {
+						count ++;
+						element[i+1][1] += element[i][1];
+						element[i+1][3] = count;
+					} else {
+						count = 1;
+						averageData.push(element[i]);
+					}
+				} else {
+					if (element[i])
+						averageData.push(element[i]);
+				}
+			}
+			currentCityInfo = [];
+			averageData.forEach(e => {
+				if (e.length === 3)
+					e[3]= 1;
+					e[1] /= e[3];
+					currentCityInfo.push(e);
+			})
+		})
+}
+
+$: worldData, updateCountries();
+
+const previous = () => {
+	if (page > 1)
+	return page --;
+}
+
+const next = () => {
+	if (page < 8)
+	return page ++;
+}
+</script>
+
+<style>
+.country-list {
+	padding-top: 10rem;
+	margin-left: auto;
+    margin-right: auto;
+	max-width: 50%;
+}
+
+.countries {
+	cursor: pointer;
+	padding: 2rem;
+	margin: 2rem;
+}
+.cities {
+	margin-left: auto;
+    margin-right: auto;
+	max-width: 50%;
+}
+.city-list-item {
+	padding: 1rem;
+	margin: 1rem;
+}
+.city-air-stats {
+	position: absolute;
+	margin-left: 10%;
+}
+</style>
---
+++ src/components/OrdinateSearch.svelte
@@ -0,0 +1,91 @@
+<div class="landing">
+	<h2>Air Quality App</h2>
+	<h3>Search Location with Latidtude and Longitude Co-Ordinates</h3>
+</div>
+<div class="search-form">
+	<input type="number" placeholder="Latitude" bind:value={ordinates.lat}/>
+	<input type="number" placeholder="Longitude" bind:value={ordinates.long}/>
+	<button on:click={search}>Search Nearest Location</button> 
+</div>
+<div class="search-result">
+{#if searchResultLocation}
+	<th>{searchResultLocation} loacted in {searchResultCity} : {searchResultCountry}</th>
+	{#each citySearchInfo as info}
+		<tr>{info[0]} : {parseFloat(info[1]).toFixed(2)} {info[2]}</tr>
+	{/each}
+	{:else}
+	{#if error}
+		<div class="warning-message">
+			The co-ordinates you entered didn't have anything to return,
+			please try again.
+		</div>
+	{/if}
+{/if}
+</div>
+
+<script>
+import axios from 'axios';
+
+let ordinates = {
+	lat: null,
+	long: null,
+};
+let citySearchInfo = [];
+let searchResults = [];
+let searchResultLocation = '';
+let searchResultCountry = '';
+let searchResultCity = '';
+let error = false;
+
+
+
+const search = () => {
+	error = false;
+	axios.get(`https://api.openaq.org/v2/locations?limit=100&coordinates=${ordinates.lat}%2C${ordinates.long}&radius=100000`)
+		.then((response) => {
+			citySearchInfo = [];
+			searchResultLocation = '';
+			searchResultCountry = '';
+			if (!response.data.results.length) {
+				error = true ;
+			} else {
+				let dataSet = response.data.results
+				let nearestIndex = null;
+				let nearestDistance = 100000;
+				dataSet.forEach((element, index)=> {
+					if(element.coordinates){
+					let distance = Math.sqrt
+					(Math.pow(element.coordinates.latitude +  ordinates.lat,2)
+					+ Math.pow(element.coordinates.longitude +  ordinates.long,2));
+					if (distance < nearestDistance) {
+						nearestIndex = index;
+						nearestDistance = distance;
+					}
+				}
+				})
+				let data = response.data.results[nearestIndex];
+				searchResultLocation = data.name;
+				searchResultCity = data.city;
+				searchResultCountry = data.country;
+				searchResults = data.parameters;
+				searchResults.forEach(element => {
+					citySearchInfo.push([element.displayName, element.average, element.unit])
+				});
+				ordinates.lat = null;
+				ordinates.long = null;
+			}
+	})		
+}
+</script>
+
+<style>
+.search-result {
+	position: absolute;
+	margin-left: 50%;
+	transform: translateX(-50%);
+	margin-top: 5rem;
+}
+.warning-message {
+	color: red;
+}
+</style>
